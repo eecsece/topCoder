6366cf355994d23e5907b63f85fe1c679a6a29db
dandanz <Dandanz@Dandanzs-MacBook-Pro.local>
2015-12-06 16:07:56 -0800

    update

--- src/test/HackerrankEnv.java
+++ src/test/HackerrankEnv.java
@@ -6,13 +6,51 @@ import java.math.*;
 import java.util.regex.*;
 
 public class HackerrankEnv {
+	static class ScoreNode{
+		int score;
+		int count;
+		ScoreNode left;
+		ScoreNode right;
+		public ScoreNode(int score){
+			this.score=score;
+			this.count=1;
+		}
+	}
+	static Map<Integer,ScoreNode> map=new HashMap<>();
+	public static String gatherScore(int[] scores){
+		ScoreNode root=new ScoreNode(scores[0]);
+		map.put(scores[0],root);
+		createTree(scores,root);
+		return bsf(root);
+		
+	}
+	private static String bsf(ScoreNode head) {
+		
+		return null;
+	}
+	private static void createTree(int[] scores,ScoreNode root){
+	
+		for(int i=1;i<scores.length;++i){
+			int score=scores[i];
+			if(map.containsKey(score)) map.get(score).count++;
+			else {
+				ScoreNode node=new ScoreNode(score);
+				if(p.left==null)
+				map.put(score, node);
+			}
+		}
+		return null;
+	}
+
 	public static void print(int n){
+		
+		
 	    for(int i=0;i<n;++i){
 	    	//int spaces=6;
 	        int spaces=n-i-1;
 	        int stars=i+1;
 	        while(spaces>0){
-	            System.out.print("*");
+	            System.out.print(" ");
 	            spaces--;
 	        }
 	        while(stars-->0){
@@ -22,6 +60,8 @@ public class HackerrankEnv {
 	    }
 	}    
     public static void main(String[] args) {
+    	
+    	
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 //        Scanner sc=new Scanner(System.in);
 //        int target=sc.nextInt();
@@ -30,6 +70,6 @@ public class HackerrankEnv {
 //        for(int i=0;i<N;++i){
 //            nums[i]=sc.nextInt();
 //        }
-    	print(6);
+//    	print(6);
     }
 }
